b72e255c34fe5502f602cfcbb403f7d42764783f
DorianRONDI <dorianrondi@gmail.com>
2023-05-09 15:52:20 +0200

    exo16 oK

--- exo16.php
+++ exo16.php
@@ -1,33 +1,35 @@
 <?php
-function isPrime(int $nbre)
-{
-    $list = [];
-    $reel = 2;
-    while (count($list) < $nbre) {
-        $prime = false;
-        for($i = 1; $i < $reel; $i++){
-
-            if($reel % $i === 0){
-                break;
-            }else{
-                $prime = true;
-            }
-        }
-
-        if ($prime){
-            $list[] = $reel;
-        }
-        $reel++;
+function isPrime(int $n) {
+    if ($n <= 1) {
+        return false;
     }
-    return $list;
+    for ($i = 2; $i < $n; $i++) {
+        if ($n % $i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function primeList($taille){
+    $liste = [];
+    $i = 2;
+    while($taille > count($liste)){
+        
+        if(isPrime($i)){
+            $liste[] = $i;
+        }
+        $i++;
+    }
+    return $liste;
 }
 
 $input = 5;
 
 if (!is_int($input) || $input < 1) {
-    echo "ERROR Not an Integer";
+    echo "ERROR";
 } else {
-    foreach (isPrime($input) as $item) {
+    foreach (primeList($input) as $item) {
         echo $item . "<br>";
     }
 }
